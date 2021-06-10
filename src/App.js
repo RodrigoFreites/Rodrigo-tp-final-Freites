1f166729af58764f0c14b732fcdd9f277e09e05b
Rodrigo Freites <79484672+RodrigoFreites@users.noreply.github.com>
2021-06-10 16:28:46 -0300

    Add files via upload
    Contador agregado

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import ItemlistContainer from './components/ItemListContainer';
-
+import ItemCount from "./components/ItemCount";
 
 function App() {
   return (
     <div className="App">
       {<NavBar/>}
       {<ItemlistContainer greeting='Hola Mundo'/>}
+      <ItemCount/>
     </div>
   );
 }
