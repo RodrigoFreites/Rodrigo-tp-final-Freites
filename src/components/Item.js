defa5ffe07c4f1cd0f617fb0fed02b536cf4b1bf
Rodrigo Freites <79484672+RodrigoFreites@users.noreply.github.com>
2021-06-17 19:04:51 -0300

    Challenge clase 7

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,40 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 
-const carrito = [
-    { id: "I-1", name: "Monitor", description: "Asus 144hz", price: 30000,pictureUrl: ""},
-    { id: "I-2", name: "Mouse", description: "Zowie EC-2", price: 5000, pictureUrl: ""},
-    { id: "I-3", name: "Teclado", description: "Redragon Draconic 60%", price: 10000, pictureUrl: ""},
-];
+function Item(props) {
+  return (
+   <div id={props.id}>
+      <h3>{props.name}</h3>
+      <p>{props.description}</p>
+      <p><small>Nos quedan {props.stock} unidades</small></p>
+   </div>
+  )
+}
 
-function getCartItems() {
-    let miPromesa = new Promise( (resolve, reject) => {
-
-        setTimeout( function() {
-            const error = Math.random() > 0.85;
-            if(!error) {
-                resolve(carrito);
-            }
-            reject("Error obteniendo los datos :(");
-        }, 200);
-    });
-
-    miPromesa.then( function(valor) {
-        console.log(valor);
-    }).catch(
-        function(error){
-            console.log(error);
-        }).finally();
-
-        return(
-            <div>
-                <button onClick={getCartItems}>Ver Productos</button>
-            </div>
-        );
-};
-
-
-
-
-export default getCartItems;
-    
+export default Item
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react'
+import Item from './Item';
+
 
 function ItemList() {
-    const posts = [
-        {
-            title: "Monitor",
-            body: "Asus 144hz",
-            id: 1,
-        },
-
-        {
-            title: "Mouse",
-            body: "Zowie EC2",
-            id: 2,
-        },
-
-        {
-            title: "Teclado",
-            body: "Redragon Draconic 60%",
-            id: 3,
-        },
+    const carrito = [
+        { id: "I-1", name: "Monitor", description: "Asus 144hz", price: 30000,pictureUrl: "", stock: 10},
+        { id: "I-2", name: "Mouse", description: "Zowie EC-2", price: 5000, pictureUrl: "", stock: 30},
+        { id: "I-3", name: "Teclado", description: "Redragon Draconic 60%", price: 10000, pictureUrl: "", stock:6},
     ];
-
-    return (
+    
+    const [products, setProducts] = useState([])
+    
+    function getCartItems() {
+        let miPromesa = new Promise( (resolve, reject) => {
+    
+            setTimeout( function() {
+                const error = false;
+                if(!error) {
+                    resolve(carrito);
+                }
+                reject("Error obteniendo los datos :(");
+            }, 200);
+        });
+    
+        miPromesa.then( function(valor) {
+            setProducts(carrito);
+        }).catch(
+            function(error){
+                console.log(error);
+            }).finally();
+    
+    };
+    
+    return(
         <div>
-            {
-                posts.map((post, index) => { return <p> id={index}  {post.title}</p>})
-            }
+            <button onClick={getCartItems}>Ver Productos</button>
+            <div>
+                {products.map( (prod)=> (
+                    <Item
+                    name={prod.name}
+                    description={prod.description}
+                    stock={prod.stock}
+                    >
+                    </Item>
+                ))}
+            </div>
+        
         </div>
     );
 }
